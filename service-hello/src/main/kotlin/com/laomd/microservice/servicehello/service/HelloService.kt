1ca7113a5941f591518a3c9afc51248e4b60ed82
laomd-2 <laomd@mail2.sysu.edu.cn>
2018-09-03 22:05:39 +0800

    服务-随机端口

--- service-hello/src/main/kotlin/com/laomd/microservice/servicehello/service/HelloService.kt
+++ service-hello/src/main/kotlin/com/laomd/microservice/servicehello/service/HelloService.kt
@@ -1,13 +1,28 @@
 package com.laomd.microservice.servicehello.service
 
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import org.springframework.boot.web.context.WebServerInitializedEvent
+import org.springframework.context.ApplicationListener
+
+@Component
+class MyApplicationListener : ApplicationListener<WebServerInitializedEvent> {
+
+    final var port: Int = 0
+        private set
+
+    override fun onApplicationEvent(event: WebServerInitializedEvent) {
+        this.port = event.webServer.port
+    }
+}
 
 @Service
 class HelloService {
-    @Value("\${server.port}")
-    val port: Int = 0
+    @Autowired
+    val listener: MyApplicationListener? = null
+
     fun hello(name: String): String {
-        return "Hello $name from $port!"
+        return "Hello $name from ${listener!!.port}!"
     }
 }
